be8dd28785b7ed11f254225f9db7ae6dacc46ea0
Ana Julia <anajvelasque@gmail.com>
2022-09-29 07:42:57 -0300

    Teste comprar livro

--- cypress/cypress/integration/spec2.js
+++ cypress/cypress/integration/spec2.js
@@ -18,5 +18,16 @@ describe('Teste End-to-End', () => {
 
         // Fecha o pop-up com o preço do frete
         cy.get('.swal-button').click()
-    })    
+    })   
+    it('Teste 4: Compra livro', () => {    
+        // Compra
+        cy.get('[data-id=3]').within(() => {
+           cy.contains('Comprar').click().then
+           cy.wait(2000)
+        })
+        cy.get('.swal-text').contains('Sua compra foi realizada com sucesso')
+
+        // Fecha o pop-up
+        cy.get('.swal-button').click()
+    }) 
   })
